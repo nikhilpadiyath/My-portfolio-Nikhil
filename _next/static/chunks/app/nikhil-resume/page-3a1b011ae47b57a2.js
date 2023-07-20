c78415f82fa2332d4c3f3373a60360638e0c2b27
nikhilpadiyath <nikhilpadiyath@gmail.com>
2023-07-20 09:26:31 +0530

    Portfolio commiting

---
+++ _next/static/chunks/app/nikhil-resume/page-3a1b011ae47b57a2.js
@@ -0,0 +1,9 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[101],{1981:function(e,r,n){Promise.resolve().then(n.bind(n,735))},735:function(e,r,n){"use strict";n.r(r),n.d(r,{default:function(){return s}});var t=n(9268);n(6006);var o=n(6008);function s(){let e=(0,o.useRouter)();return(0,t.jsxs)("div",{children:[(0,t.jsx)("div",{children:(0,t.jsx)("h2",{className:"text-purple-800 cursor-pointer py-2",onClick:()=>e.push("/"),children:"Back to Home"})}),(0,t.jsx)("div",{children:(0,t.jsx)("a",{href:"/Resume_Nikhil.pdf",target:"_blank",children:"My Resume"})})]})}},3177:function(e,r,n){"use strict";/**
+ * @license React
+ * react-jsx-runtime.production.min.js
+ *
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var t=n(6006),o=Symbol.for("react.element"),s=(Symbol.for("react.fragment"),Object.prototype.hasOwnProperty),u=t.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,i={key:!0,ref:!0,__self:!0,__source:!0};function c(e,r,n){var t,c={},f=null,l=null;for(t in void 0!==n&&(f=""+n),void 0!==r.key&&(f=""+r.key),void 0!==r.ref&&(l=r.ref),r)s.call(r,t)&&!i.hasOwnProperty(t)&&(c[t]=r[t]);if(e&&e.defaultProps)for(t in r=e.defaultProps)void 0===c[t]&&(c[t]=r[t]);return{$$typeof:o,type:e,key:f,ref:l,props:c,_owner:u.current}}r.jsx=c,r.jsxs=c},9268:function(e,r,n){"use strict";e.exports=n(3177)},6008:function(e,r,n){e.exports=n(8170)}},function(e){e.O(0,[253,961,744],function(){return e(e.s=1981)}),_N_E=e.O()}]);
